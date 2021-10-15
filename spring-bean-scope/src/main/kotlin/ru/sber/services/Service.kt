80ff81574ec54483d60b3ba83a75c07d0cbef4b2
Artem <viktorzebra54@gmail.com>
2021-10-15 14:16:53 +0300

    scope is done

---
+++ spring-bean-scope/src/main/kotlin/ru/sber/services/Service.kt
@@ -0,0 +1,12 @@
+package ru.sber.services
+
+import org.springframework.context.annotation.Scope
+import org.springframework.stereotype.Component
+
+@Component
+@Scope("singleton")
+class SingletonService
+
+@Component
+@Scope("prototype")
+class PrototypeService
